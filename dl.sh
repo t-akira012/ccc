b50d810583c2b3fb6618906c9cb97539a0c81874
t-akira012 <aki003xyz@gmail.com>
2025-06-17 21:58:33 +0900

    update

--- dl.sh
+++ dl.sh
@@ -4,5 +4,5 @@ cd $(dirname $0)
 
 curl -L "https://github.com/t-akira012/claude-code-compose/archive/main.tar.gz" --output main.tar.gz
 tar -xf main.tar.gz
-mv ./claude-code-compose-main/ ./.claude-compose/
+mv claude-code-compose-main .ccc
 rm -f main.tar.gz
