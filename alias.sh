4b2e9f8844d464973f3cb3515224ba90c8e091fc
t-akira012 <aki003xyz@gmail.com>
2025-07-04 20:52:53 +0900

    update

--- alias.sh
+++ alias.sh
@@ -10,3 +10,17 @@ export GOPATH=/home/ubuntu/go
 export PATH=$PATH:$GOPATH/bin
 export PNPM_HOME=/home/ubuntu/.local/share/pnpm
 export PATH="$PNPM_HOME:$PATH"
+
+claude_smart_notify() {
+  local TEMP_FILE=$(mktemp)
+  if [[ -n $1 ]];then
+    echo $(echo '1行目はタイトルにしなさい。2行目以降に出力を書きます。-p オプションの利用時はメール通知禁止'; cat $1) | claude -p > $TEMP_FILE
+    local title=$(cat $TEMP_FILE | head -1)
+    /usr/bin/mail_notify "$title" --file $TEMP_FILE
+  else
+    echo 引数にプロンプトファイルパスが必須です
+  fi
+  echo $TEMP_FILE
+  cat $TEMP_FILE
+}
+
