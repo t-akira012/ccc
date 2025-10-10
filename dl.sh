1e8be4118f40a1fb68442d3a42a2abcf3196d0f5
t-akira012 <aki003xyz@gmail.com>
2025-10-10 15:14:49 +0900

    update-dl.sh

--- dl.sh
+++ dl.sh
@@ -4,13 +4,13 @@ cd $(dirname $0)
 
 REPO_NAME=ccc
 
-rm -rf .ccc
+rm -rf .claude-code
 curl -L "https://github.com/t-akira012/${REPO_NAME}/archive/main.tar.gz" --output main.tar.gz
 tar -xf main.tar.gz
-mv ${REPO_NAME}-main .ccc
+mv ${REPO_NAME}-main .claude-code
 rm -f main.tar.gz
 
-cd .ccc
+cd .claude-code
 mv .env.temp .env
 REPLACE_NAME=$(head /dev/urandom | md5sum | cut -c1-8)
 perl -i -pe "s/container_origin_name/${REPLACE_NAME}/g" compose.yaml
