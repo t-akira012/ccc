5b21b7da6621f768f13e6b380fb89bfe6230248e
t-akira012 <aki003xyz@gmail.com>
2025-12-26 23:05:57 +0900

    update

--- bashrc-ex.sh
+++ bashrc-ex.sh
@@ -1,5 +1,6 @@
 alias ccc='claude'
 alias cca='claude auth'
 alias ccd='claude --dangerously-skip-permissions'
+alias ccu='echo "Update Claude Code" && sudo npm update -g @anthropic-ai/claude-code'
 export CLAUDE_CONFIG_DIR=/home/ubuntu/.config/claude
 export GEMINI_CONFIG_DIR=/home/ubuntu/.config/gemini
