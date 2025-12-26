9988c548cfa9bc70bfef77b0b7f59c4803e6dbcd
t-akira012 <aki003xyz@gmail.com>
2025-12-27 01:40:27 +0900

    update

--- bashrc-ex.sh
+++ bashrc-ex.sh
@@ -1,8 +1,8 @@
 alias ccc='claude'
 alias cca='claude auth'
 alias ccd='claude --dangerously-skip-permissions'
-alias ccu='echo "Update Claude Code" && sudo npm update -g @anthropic-ai/claude-code'
-alias update_all='echo "Update AI Agents" && sudo npm update -g @anthropic-ai/claude-code @openai/codex @google/gemini-cli'
+alias ccu='echo "Update Claude Code" && npm update -g @anthropic-ai/claude-code'
+alias update_all='echo "Update AI Agents" && npm update -g @anthropic-ai/claude-code @openai/codex @google/gemini-cli'
 export CLAUDE_CONFIG_DIR=/home/ubuntu/.config/claude
 export GEMINI_CONFIG_DIR=/home/ubuntu/.config/gemini
 
