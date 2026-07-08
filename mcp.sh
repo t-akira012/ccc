2b8c3e4733779ee5d634d0342894f5c8fe496e22
t-akira012 <aki003xyz@gmail.com>
2026-07-08 11:43:02 +0900

    update

--- mcp.sh
+++ mcp.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-claude mcp add aws-documentation-mcp-server uvx awslabs.aws-documentation-mcp-server@latest
+:
+# claude mcp add aws-documentation-mcp-server uvx awslabs.aws-documentation-mcp-server@latest
 # claude mcp add serena -- uvx --from git+https://github.com/oraios/serena serena start-mcp-server --context ide-assistant --project /workspace
